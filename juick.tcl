bfe55278a860fa50612c97b05de07e2c5a6c563e
Totktonada <alex@TKN_Notebook.(none)>
2009-12-06 23:43:02 +0300

    Added: 1. Copy things to clipboard (needed tests). 2. Russian translate (one line).
    Fixed:
    1. View smiles which as *THUMBS UP*.
    2. Small fixes in regexp (in process using).
    3. Parallel render things and citate.
    4. Change render priority:
    4.1. Fised history.
    4.2. Fixed number new messages in juick tab.
    4.3. Probably, sound. Etc.

--- juick.tcl
+++ juick.tcl
@@ -1,9 +1,12 @@
+package require msgcat
+
 namespace eval juick {
 
 # Determines whether given chatid correspond to Juick
 proc is_juick {chatid} {
     set jid [chat::get_jid $chatid]
-#    return [cequal $jid "juick@juick.com/Juick"]
+#    set jid [chat::get_jid [chat::winid_to_chatid [join [lrange [split $w .] 0 end-1] .]]]
+
      return [expr [cequal $jid "juick@juick.com/Juick"] || [regexp "juick%juick.com@.+/Juick" $jid]]
 }
 
@@ -12,28 +15,23 @@ proc handle_message {chatid from type body x} {
         ::richtext::property_add {JUICK} {}
         set chatw [chat::chat_win $chatid]
         set jid [chat::get_jid $chatid]
-        $chatw tag configure JNICK -foreground red
-        $chatw tag configure JTAG -foreground ForestGreen
-        $chatw tag configure JNUM -foreground blue
-        $chatw tag configure JCITING -foreground gray35
-        $chatw tag configure JMY -foreground gray
-        if {[cequal $jid $from]} {
-            ::richtext::render_message $chatw $body {}
-        } else {
-            ::richtext::render_message $chatw $body JMY
+
+        set tags {}
+        if {![cequal $jid $from]} {
+            lappend tags JMY
         }
+
+        ::richtext::render_message $chatw $body $tags
         return stop
     }
 }
-
-hook::add draw_message_hook [namespace current]::handle_message 10
+hook::add draw_message_hook [namespace current]::handle_message 21
 
 proc ignore_server_messages {chatid from type body x} {
     if {[is_juick $chatid] && $from == ""} {
         return stop;
     }
 }
-
 hook::add draw_message_hook [namespace current]::ignore_server_messages 0
 
 proc insert_from_window {chatid w x y} {
@@ -51,11 +49,28 @@ proc insert_from_window {chatid w x y} {
     $ci insert insert "$thing "
     return stop
 }
+hook::add chat_window_click_hook [namespace current]::insert_from_window
 
-hook::add chat_window_click_hook \
-    [namespace current]::insert_from_window
+proc add_juick_things_menu {m chatwin X Y x y} {
+    set thing ""
+    set tags [$chatwin tag names "@$x,$y"]
 
-variable commands {HELP NICK LOGIN S U ON OFF D BL WL PM VCARD PING}
+    if {[set idx [lsearch -glob $tags JUICK-*]] >= 0} {
+        set thing [string range [lindex $tags $idx] 6 end]
+    }
+
+    if {$thing == ""} return
+    $m add command -label [::msgcat::mc "\[J\] Copy thing to clipboard."] \
+          -command [list [namespace current]::copy_thing $chatwin $thing]
+}
+hook::add chat_win_popup_menu_hook [namespace current]::add_juick_things_menu 20
+
+proc copy_thing {w thing} {
+    clipboard clear -displayof $w
+    clipboard append -displayof $w $thing
+}
+
+variable commands {HELP NICK LOGIN S U ON OFF D BL WL PM VCARD PING INVITE}
 proc correct_command {chatid user body type} {
    # Maybe once I'll get arount to it 
 }
@@ -63,95 +78,127 @@ proc correct_command {chatid user body type} {
 # --------------
 # RichText stuff
 
-proc configure_richtext_widget {w} {
+proc configure_juick {w} {
     $w tag configure JNICK -foreground red
     $w tag configure JTAG -foreground ForestGreen
     $w tag configure JNUM -foreground blue
-    $w tag configure JCITING -foreground gray35
     $w tag configure JMY -foreground gray
 }
 
-proc spot {what at startVar endVar} {
+proc configure_citing {w} {
+    $w tag configure CITING -foreground gray35
+}
+
+proc spot_citing {what at startVar endVar} {
     set matched [regexp -indices -start $at -- \
-    {(?:\s|\n|\A|\()(#\d+(/\d+)?|@[\w@.-]+|\*[\w?+'.-]+|>[^\n]+)} $what -> bounds]
-#    set matched [expr [regexp -indices -start $at -- \
-#    {(?:\s|\n|\A|\()(#\d+(/\d+)?|@[\w@.-]+|\*[\w?.-]+)} $what -> bounds] || \
-#                      [regexp -indices -start $at -- \
-#    {(?:\n|\A)(>[^\n]+)} $what -> bounds]]
+    {(?:\n|\A)(>[^\n]+)} $what -> bounds]
 
     if {!$matched} { return false }
 
-    upvar 1 $startVar us $endVar ue
-    lassign $bounds us ue
+    upvar 1 $startVar uStart $endVar uEnd
+    lassign $bounds uStart uEnd
     return true
 }
 
-proc process {atLevel accName} {
+proc spot_juick {what at startVar endVar} {
+    set matched [regexp -indices -start $at -- \
+    {(?:\s|\n|\A|\(|\>)(#\d+(/\d+)?|@[\w@.-]+|\*[\w?!+'/.-]+)(?:(\.(\s|\n))?)} $what -> bounds]
+
+    if {!$matched} { return false }
+
+    upvar 1 $startVar uStart $endVar uEnd
+    lassign $bounds uStart uEnd
+    return true
+}
+
+proc process_juick {atLevel accName} {
+return [process $atLevel $accName juick]
+}
+
+proc process_citing {atLevel accName} {
+return [process $atLevel $accName citing]
+}
+
+proc process {atLevel accName what} {
     upvar #$atLevel $accName chunks
 
     if {![::richtext::property_exists {JUICK}]} {return}
-
     set out {}
 
     foreach {s type tags} $chunks {
-        if {$type != "text"} {
+        if {[lsearch -regexp $type (text)|(citing)]<0} {
             # pass through
             lappend out $s $type $tags
             continue
         }
 
-        set ix 0; set us 0; set ue 0
-        
-        while {[spot $s $ix us ue]} {
-            if {$us - $ix > 0} {
-                # dump chunk before juick
-                lappend out [string range $s $ix [expr {$us - 1}]] $type $tags
+        if {[expr [lsearch -exact $type citing]>=0]} {
+        lappend tags CITING
+        }
+
+        set index 0; set uStart 0; set uEnd 0
+        while {[eval {spot_$what $s $index uStart uEnd}]} {
+            if {$uStart - $index > 0} {
+                # Write out text before current thing, if any:
+                lappend out [string range $s $index [expr {$uStart - 1}]] $type $tags
             }
 
-            set thing [string range $s $us $ue]
-
-            lappend out $thing juick $tags
-
-            set ix [expr {$ue + 1}]
+            set thing [string range $s $uStart $uEnd]
+            # Write out current thing:
+            lappend out $thing $what $tags
+            set index [expr {$uEnd + 1}]
         }
-
-        if {[string length $s] - $ix > 0} {
-        lappend out [string range $s $ix end] $type $tags
+        # Write out text after the last thing, if any:
+        if {[string length $s] - $index > 0} {
+        lappend out [string range $s $index end] $type $tags
         }
     }
-
     set chunks $out
 }
 
-proc render {w type thing tags args} {
-    if {[cequal [string index $thing 0] "#" ]} {
-        set type JNUM
-        } else {
-               if {[cequal [string index $thing 0] "*" ]} {
-                  set type JTAG
-                  } else {
-                         if {[cequal [string index $thing 0] "@" ]} {
-                            set type JNICK
-                            }
-                         }
-               }
+proc render_juick {w type thing tags args} {
+    if {[lsearch -exact $tags CITING]<0} {
+       if {[cequal [string index $thing 0] "#" ]} {
+          set type JNUM
+          } else {
+                 if {[cequal [string index $thing 0] "*" ]} {
+                    set type JTAG
+                    } else {
+                           if {[cequal [string index $thing 0] "@" ]} {
+                               set type JNICK
+                               }
+                           }
+                 }
+    } else {
+           set type CITING
+           }
+
     set id JUICK-$thing
-
-    if {[cequal [string index $thing 0] ">" ]} {
-       set type JCITING
-       set id CITING-$thing
-       }
-
     $w insert end $thing [lfuse $tags [list $id $type JUICK]]
     return $id
 }
 
-::richtext::register_entity juick \
-    -configurator [namespace current]::configure_richtext_widget \
-    -parser [namespace current]::process \
-    -renderer [namespace current]::render \
-    -parser-priority 60
+proc render_citing {w type thing tags args} {
+    set id CITING-$thing
+    $w insert end $thing [lfuse $tags [list $id $type CITING]]
+    return $id
+}
 
+::msgcat::mcload [file join [file dirname [info script]] msgs]
+
+::richtext::register_entity juick \
+    -configurator [namespace current]::configure_juick \
+    -parser [namespace current]::process_juick \
+    -renderer [namespace current]::render_juick \
+    -parser-priority 85
+
+::richtext::register_entity citing \
+    -configurator [namespace current]::configure_citing \
+    -parser [namespace current]::process_citing \
+    -renderer [namespace current]::render_citing \
+    -parser-priority 82
+
+    ::richtext::entity_state citing 1
     ::richtext::entity_state juick 1
 }
 # vi:ts=4:et
