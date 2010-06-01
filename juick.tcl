c495ae43d492caad006658bf5f0f7f10862264bc
Totktonada <alextux@bk.ru>
2010-06-02 01:30:45 +0400

    Small bugfixes * Fix error "can't read "entities(juick_numbers,enabled)": no such element in array". * Fix focus in Juick tab on insert things from Tkabber r1869. * Fix open numbers (#dddddd/dd) in browser. * Add newline to end colored private message.

--- juick.tcl
+++ juick.tcl
@@ -12,7 +12,6 @@ hook::add chat_win_popup_menu_hook [namespace current]::add_juick_things_menu 20
 # Determines whether given chatid correspond to Juick
 proc is_juick {chatid} {
     set jid [chat::get_jid $chatid]
-#    set jid [chat::get_jid [chat::winid_to_chatid [join [lrange [split $w .] 0 end-1] .]]]
     return [expr [cequal $jid "juick@juick.com/Juick"] || [regexp "juick%juick.com@.+/Juick" $jid]]
 }
 
@@ -51,6 +50,7 @@ proc insert_from_window {chatid w x y} {
     if {$thing == ""} return
 
     $ci insert insert "$thing "
+    focus -force $ci
     return stop
 }
 
@@ -76,7 +76,11 @@ proc copy_thing {w thing} {
 
 proc browse_thing {w thing} {
     switch -regexp -- $thing {
-        {^[#@]} {
+        {^#} {
+          regsub -- "/" [string range $thing 1 end] "#" jurl
+          browseurl http://juick.com/$jurl
+          }
+        {^@} {
           browseurl http://juick.com/[string range $thing 1 end]
           }
         {^\*} {
@@ -248,7 +252,7 @@ proc render_citing {w type thing tags args} {
 
 proc render_juick_ligth {w type thing tags args} {
     set id JLIGTH-$thing
-    $w insert end $thing [lfuse $tags [list $id $type JLIGTH]]
+    $w insert end [append thing \n] [lfuse $tags [list $id $type JLIGTH]]
     return $id
 }
 
@@ -278,5 +282,7 @@ proc render_juick_ligth {w type thing tags args} {
 
 ::richtext::entity_state citing 1
 ::richtext::entity_state juick 1
+::richtext::entity_state juick_numbers 1
+::richtext::entity_state juick_ligth 1
 }
 # vi:ts=4:et
